3519790ff27221cd7e831814940b80e294ae6b31
larrytamnjong <larrytabeh@gmail.com>
2026-02-25 12:24:49 +0100

    Add project files.

---
+++ ParquetSharpBenchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    //static void Main(string[] args)
+    //{
+    //    BenchmarkRunner.Run<ParquetDotNetBenchmarks>();
+    //}
+
+    static async Task Main(string[] args)
+    {
+        await ParquetBenchmarks.RunAsync(args);
+    }
+}
